53c2513a5e113835ccc7938c1773bb08cb1a2191
Mohammed Nasif <m.khaled100@ymail.com>
2023-02-14 16:10:04 +0200

    moke hotels API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import './App.css';
+import { getHotelsData } from './apis/getHotelsData';
+import { useEffect, useState } from 'react';
 
 function App() {
+	const [hotelsData, setHotelsData] = useState([]);
+	useEffect(() => {
+		const controller = new AbortController();
+		const signal = controller.signal;
+
+		(async () => {
+			const data = await getHotelsData(signal);
+			data.sort((a, b) => b.hotelRating - a.hotelRating);
+			setHotelsData(data);
+		})();
+
+		return () => {
+			controller.abort('Cancel Previous Request');
+		};
+	}, []);
+
 	return (
 		<div className='App'>
 			<header className='App-header'>Where To Go !</header>
